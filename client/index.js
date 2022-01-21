8d3184838a2febade90bf9875da3947194ce2484
reburke286 <reburke286@gmail.com>
2022-01-21 16:14:22 -0500

    added express package and updated index.js

--- client/index.js
+++ client/index.js
@@ -10,12 +10,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/amazon", {
-  useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-mongoose.set("useCreateIndex", true);
+// mongoose.set("useCreateIndex", true);
 mongoose.set("debug", true);
 
 app.listen(PORT, () => {
---
+++ client/models/index.js
@@ -0,0 +1,4 @@
+const Product_Info = require("./Product_Info");
+const Reviews = require("./Reviews");
+
+module.exports = { Product_Info, Reviews };
